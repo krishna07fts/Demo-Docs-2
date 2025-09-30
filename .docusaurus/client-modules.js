c563390d50894bb9422ce7a44fd4327d10b4336f
krishna07fts <m.krishna@faaztechsolutions.com>
2025-09-30 18:52:07 +0530

    Deploy website - based on 118a4b02faf74b6deec21a757f2ff391a557a034

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("D:\\krishna\\Docusaurus\\demo-docs\\my-website\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("D:\\krishna\\Docusaurus\\demo-docs\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\krishna\\Docusaurus\\demo-docs\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\krishna\\Docusaurus\\demo-docs\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\krishna\\Docusaurus\\demo-docs\\my-website\\src\\css\\custom.css"),
-];
